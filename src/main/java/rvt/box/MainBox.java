42375deef67a43d6a22d5a5bec4d27b8307e882c
a240265nk <a240265nk@rvt.lv>
2026-03-17 14:07:37 +0200

    Finish "Interface in a Box" - make box packable, add verification to prevent box overloading

--- src/main/java/rvt/box/MainBox.java
+++ src/main/java/rvt/box/MainBox.java
@@ -1,9 +1,10 @@
 package rvt.box;
 import java.util.*;
 
-public class Main {
+public class MainBox {
     public static void main(String[] args) {
         Box box = new Box(10);
+        Box anotherBox = new Box(5);
 
         box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2)) ;
         box.add(new Book("Robert Martin", "Clean Code", 1));
@@ -13,6 +14,15 @@ public class Main {
         box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
         box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
 
-        System.out.println();
+        //box.add(new Book("Aleksandr Solzhenitsyn", "All Memoirs", 7));
+
+        anotherBox.add(new CD("Toby Fox", "Megalovania", 2015));
+        anotherBox.add(new CD("Toby Fox", "Another Medium", 2015));
+        anotherBox.add(new CD("Toby Fox", "Spider Dance", 2015));
+        anotherBox.add(new CD("Toby Fox", "Howdy!", 2015));
+
+        box.add(anotherBox);
+
+        System.out.println(box);
     }
 }
